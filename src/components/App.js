fe779599471efa9a536798fe07cc0e3200908658
Vishnu <bandarivishnu143@gmail.com>
2025-05-20 15:41:12 +0530

    don

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,50 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { Route, Switch, Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <h1>Item List</h1>
+        <ul>
+          <li>
+            <Link to="/item/1">Item1</Link>
+          </li>
+          <li>
+            <Link to="/item/2">Item2</Link>
+          </li>
+          <li>
+            <Link to="/item/3">Item3</Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+const ItemDetails = () => {
+  // const {id}=useParams()
+  const don = window.location.pathname.split("/").reverse()[0];
+  return (
+    <>
+      <h1>Item {don}</h1>
+      <p>Discription for Item {don}</p>
+    </>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/item/:id">
+        <ItemDetails />
+      </Route>
+    </Switch>
+  );
+};
 
-export default App
+export default App;
